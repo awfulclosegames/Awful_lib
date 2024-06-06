aa6fbc78db7526a508ab0adcc18a4a007e502fbd
random <randomise@gmail.com>
2024-06-06 18:26:30 +0200

    Example level/char of using spline for movement

--- SDZ_Lib/Source/SDZ_Lib/Private/Character/SplineTestCharacter.cpp
+++ SDZ_Lib/Source/SDZ_Lib/Private/Character/SplineTestCharacter.cpp
@@ -35,7 +35,7 @@ ASplineTestCharacter::ASplineTestCharacter(const FObjectInitializer& ObjectIniti
 	// instead of recompiling to adjust them
 	GetCharacterMovement()->JumpZVelocity = 700.f;
 	GetCharacterMovement()->AirControl = 0.35f;
-	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MaxWalkSpeed = 600.f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 
